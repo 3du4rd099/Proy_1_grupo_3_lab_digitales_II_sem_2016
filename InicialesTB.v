8e4f881be784001f2387bb89ee2a8d3401cd8469
Carlosa215 <carlosagg9519@gmail.com>
2016-08-18 21:57:34 -0600

    Add files via upload

---
+++ InicialesTB.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   18:14:29 08/18/2016
+// Design Name:   Iniciales
+// Module Name:   C:/Users/Carlos Gomez/Xilinx/SysGen/14.7/VGA_Controller/InicialesTB.v
+// Project Name:  VGA_Controller
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Iniciales
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module InicialesTB;
+
+	// Inputs
+	reg clk;
+	reg [9:0] pix_x;
+	reg [9:0] pix_y;
+
+	// Outputs
+	wire [3:0] text_on;
+	wire [2:0] text_rgb;
+	
+	// Instantiate the Unit Under Test (UUT)
+	Iniciales uut (
+		.clk(clk), 
+		.pix_x(pix_x), 
+		.pix_y(pix_y), 
+		.text_on(text_on), 
+		.text_rgb(text_rgb)
+	);
+	reg hmax;
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		pix_x = 0;
+		pix_y = 0;
+		hmax = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+	always begin
+	#20 clk =! clk;
+	end
+	always @(posedge clk) 
+      if (pix_x==800)begin
+         pix_x <=0; pix_y <= pix_y + 1;
+      end else if(pix_y==525) begin
+         pix_y <= 0;
+			end
+		else begin
+			pix_x <= pix_x + 1;
+				end
+endmodule
+
---
+++ font_romTB.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:28:51 08/18/2016
+// Design Name:   font_ROM
+// Module Name:   C:/Users/Carlos Gomez/Xilinx/SysGen/14.7/VGA_Controller/font_romTB.v
+// Project Name:  VGA_Controller
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: font_ROM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module font_romTB;
+
+	// Inputs
+	reg clk;
+	reg [10:0] addr;
+
+	// Outputs
+	wire [7:0] data;
+
+	// Instantiate the Unit Under Test (UUT)
+	font_ROM uut (
+		.clk(clk), 
+		.addr(addr), 
+		.data(data)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		addr = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+	end
+	always begin
+	#20 clk =! clk;
+	end
+	always @(posedge clk) 
+      if (addr==11'h7ff)
+         addr <= 0;
+      else 
+         addr <= addr + 1;
+
+endmodule
+
