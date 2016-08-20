c92ff6b9618b36defd57f2688afe252f2729d8f1
Carlosa215 <carlosagg9519@gmail.com>
2016-08-19 21:34:46 -0600

    Update InicialesTB.v

--- InicialesTB.v
+++ InicialesTB.v
@@ -25,29 +25,50 @@
 module InicialesTB;
 
 	// Inputs
-	reg clk;
+	reg clk, Black, Blue, Green, Cyan, Red, Magenta, Yellow, White;
 	reg [9:0] pix_x;
 	reg [9:0] pix_y;
+	reg [18:0] tiempo;
+	reg [2:0] color;
 
 	// Outputs
 	wire [3:0] text_on;
 	wire [2:0] text_rgb;
+	wire hsync, vsync;
 	
 	// Instantiate the Unit Under Test (UUT)
 	Iniciales uut (
-		.clk(clk), 
+		.clk(clk),
+		.Black(Black),
+		.Blue(Blue),
+		.Green(Green),
+		.Cyan(Cyan),
+		.Red(Red),
+		.Magenta(Magenta),
+		.Yellow(Yellow),
+		.White(White),
 		.pix_x(pix_x), 
 		.pix_y(pix_y), 
 		.text_on(text_on), 
 		.text_rgb(text_rgb)
 	);
-	reg hmax;
+
+
 	initial begin
 		// Initialize Inputs
 		clk = 0;
 		pix_x = 0;
 		pix_y = 0;
-		hmax = 0;
+		Black = 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+		tiempo = 0;
+		color = 0;
 
 		// Wait 100 ns for global reset to finish
 		#100;
@@ -66,6 +87,103 @@ module InicialesTB;
 			end
 		else begin
 			pix_x <= pix_x + 1;
-				end
+		end
+	always @(posedge clk) 
+      if (tiempo==420000)begin
+         tiempo <=0; color <= color + 1;
+      end else if (color == 8) begin
+         color = 0;
+			end
+			else begin
+			tiempo <= tiempo + 1;
+			end
+	always @(posedge clk)
+	case (color)
+      3'b000 : begin
+                Black= 1;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b001 : begin
+      		Black= 0;
+		Blue = 1;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b010 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 1;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b011 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 1;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b100 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 1;
+		Magenta = 0;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b101 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 1;
+		Yellow = 0;
+		White = 0;
+               end
+      3'b110 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 1;
+		White = 0;
+               end
+      3'b111 : begin
+                Black= 0;
+		Blue = 0;
+		Green = 0;
+		Cyan = 0;
+		Red = 0;
+		Magenta = 0;
+		Yellow = 0;
+		White = 1;
+               end
+      default: begin
+                  Black=1;
+               end
+   endcase
+	
 endmodule
+
 
